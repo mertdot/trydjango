48711c313ba5abf457712bdea14c8408b58f312b
mertdot <mertdot@icloud.com>
2020-03-20 20:48:02 +0300

    finished

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,21 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title   = forms.CharField(label='Title',
+                widget=forms.TextInput(attrs={"placeholder": "Your Title"}))
+    email   = forms.EmailField()
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+        attrs={
+            "placeholder": "type a description",
+            "class": "new-class-name",
+            "rows": 1 ,
+            'cols': 20
+        }
+        )
+)
+    price       = forms.DecimalField(initial=199.99) 
     class Meta:
         model = Product
         fields = [
@@ -10,6 +25,22 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'  
         ]
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError("This is not a valid title")
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("title")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not edu mail!")
+            return email
+
+
+
+
 
 class RawProductForm(forms.Form):
     title = forms.CharField(
--- src/products/views.py
+++ src/products/views.py
@@ -3,23 +3,23 @@ from django.shortcuts import render
 from .models import Product
 from .forms import ProductForm, RawProductForm
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            #Django sure to data is trusted
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form
-    }
-    return render(request, "products/product_create.html", context)
- 
-
 
 """ def product_create_view(request):
+        my_form = RawProductForm()
+        if request.method == "POST":
+            my_form = RawProductForm(request.POST)
+            if my_form.is_valid():
+                #Django sure to data is trusted
+                Product.objects.create(**my_form.cleaned_data)
+            else:
+                print(my_form.errors)
+        context = {
+            "form": my_form
+        }
+        return render(request, "products/product_create.html", context)
+ """
+ 
+def product_create_view(request):
     form = ProductForm(request.POST or None)
     if form.is_valid():
         form.save()
@@ -30,7 +30,7 @@ def product_create_view(request):
     }
 
     return render(request, "products/product_create.html", context)
- """
+
 
 
 
@@ -49,3 +49,14 @@ def product_detail_view(request):
     }
 
     return render(request, "products/product_detail.html", context)
+
+    def render_initial_data(request):
+        initial_data = {
+            'title': "My awesome title"
+            }
+        form = RawProductForm(request.POST or None, initial=initial_data)
+        context = {
+            'form' : form
+        }
+
+        return render(request, "products/product_create.html", context)
